fb202fad48ca2f9c74c89ed410b65f1f45a80500
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2023-10-08 20:47:02 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,58 +9,10 @@ import sass from './assets/images/sass.png';
 import ts from "./assets/images/typescript.png"
 import resume from "./assets/images/resume.png"
 import projects from "./data"
-import { useState } from "react";
 import resumePDF from "./assets/Resume.pdf"
-import toast, {Toaster} from "react-hot-toast"
 
 function App() {
-  const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-	const [message, setMessage] = useState('');
-	const [errors, setErrors] = useState({});
 
-	function handleInputChange(event) {
-		const { name, value } = event.target;
-		if (name === 'name') {
-			setName(value);
-		} else if (name === 'email') {
-			setEmail(value);
-		} else if (name === 'message') {
-			setMessage(value);
-		}
-		setErrors({});
-	}
-
-	function handleSubmit(event) {
-		event.preventDefault();
-
-		let formErrors = {};
-
-		if (name.trim() === '') {
-			formErrors.name = 'Name is required';
-		}
-
-		if (email.trim() === '') {
-			formErrors.email = 'Email is required';
-		}
-
-		if (message.trim() === '') {
-			formErrors.message = 'Message is required';
-		}
-
-		if (Object.keys(formErrors).length > 0) {
-			// There are errors, display them to the user and prevent form submission
-			setErrors(formErrors);
-			return;
-		}
-
-		// Call your API or do whatever else you need to do to submit the form
-		console.log('Submitting form...');
-		setEmail('');
-		setMessage('');
-		setName('');
-		toast.success("Thanks!,  I'll be in touch soon!");
-	}
 
 const projectsList = projects.map(project => {
 return (
@@ -89,7 +41,6 @@ return (
 return (
 	<>
 		<main>
-			<Toaster />
 			<section className='logo-section'>
 				<h1 className='logo'>braxtonjones</h1>
 				<div className='icons'>
@@ -112,54 +63,30 @@ return (
 					Nice to meet you! I'm <span>Braxton Jones.</span>
 				</h1>
 				<p className='hero-content'>
-					Based in the US, I'm a front-end developer with a love for building
-					web apps that add value to thier users.{' '}
+				I build accessible, inclusive and unique experiences for the web.
 				</p>
 				<a href='mailto:braxtonjonesttsl@gmail.com' className='underline'>
-					Get in touch
+				Get in touch (Open to Opportunities)
 				</a>
 			</section>
 			<section className='about-me-container'>
 				<div>
 					<h2 className='about-me-title'>About Me</h2>
 					<p>
-						Hello! I'm Braxton Jones, a self-taught front-end developer with a
-						deep passion for technology and web design. I've spent countless
-						hours perfecting my skills and creating web applications that marry
-						functionality with aesthetics. In my free time, I love exploring the
-						latest UX/UI trends, which helps me approach web development from a
-						well-rounded perspective.
+					In 2018, I made the decision to take a 
+					leap into learning web development and 
+					transition away from my apprenticeship in 
+					system administration. Since then, I've 
+					been actively exploring new technologies and 
+					acquiring skills to effectively transform 
+					abstract concepts into tangible realities. 
+					I am a self-starter and self-sufficient, 
+					but I also excel in collaboration. 
+					I maintain a strong sense of self-motivation
+					 and possess excellent communication and interpersonal skills.
 					</p>
-				</div>
-				<div>
-					<h3>My Toolkit</h3>
-					<ul>
-						<li>
-							<img src={javascript} className='toolkit-icon' />
-							Javacript (ES6+)
-						</li>
-						<li>
-							<img src={react} className='toolkit-icon' />
-							React
-						</li>
-						<li>
-							<img src={node} className='toolkit-icon' />
-							Node.js
-						</li>
-						<li>
-							<img src={figma} className='toolkit-icon' />
-							Figma
-						</li>
-						<li>
-							<img src={sass} className='toolkit-icon' />
-							Sass
-						</li>
-						<li>
-							<img src={ts} className='toolkit-icon' />
-							Typescript
-						</li>
-					</ul>
-				</div>
+					<p className="accent">At the heart of all my pursuits lies the spark of creation.</p>
+				</div>	
 			</section>
 			<section className='projects-container'>
 				<h3>What I've been working on.</h3>
--- src/data.js
+++ src/data.js
@@ -1,9 +1,5 @@
-import dine from "./assets/images/dine.png";
 import arch from "./assets/images/arch-studio.png";
-import devjobs from "./assets/images/devjobs.png";
 import markdown from "./assets/images/markdown.png";
-import artGallery from "./assets/images/art-gallery.png"
-import dineDiner from "./assets/images/dine-diner.png"
 const projects = [
 	{
 		name: 'Markdown Web App',
@@ -13,39 +9,11 @@ const projects = [
 		'website-link': 'https://braxton-markdown-app.netlify.app/',
 	},
 	{
-		name: 'Devjobs Web app',
-		image: devjobs,
-		tags: ['HTML', 'Sass', 'React'],
-		'code-link': 'https://github.com/Braxton-Jones/job-board-app',
-		'website-link': 'https://braxton-job-board-app.netlify.app/',
-	},
-	{
 		name: 'Arch Studio Landing Page',
 		image: arch,
 		tags: ['HTML', 'Sass', 'React'],
 		'code-link': 'https://github.com/Braxton-Jones/arch-studio',
 		'website-link': 'https://arch-studios-landing-page.netlify.app/',
-	},
-	{
-		name: 'Art Gallery Showcase',
-		image: artGallery,
-		tags: ['HTML', 'CSS'],
-		'code-link': 'https://github.com/Braxton-Jones/art-gallery-page',
-		'website-link': 'https://braxton-jones.github.io/art-gallery-page/',
-	},
-	{
-		name: 'Dine Reservation(Customer) Web App',
-		image: dine,
-		tags: ['HTML', 'Sass', 'React', 'Node'],
-		'code-link': 'https://github.com/Braxton-Jones/reservation-customer-app',
-		'website-link': 'https://braxton-dine-reservation.netlify.app/',
-	},
-	{
-		name: 'Dine Reservation(Diner) Web App',
-		image: dineDiner,
-		tags: ['HTML', 'Sass', 'React', 'Node'],
-		'code-link': 'https://github.com/Braxton-Jones/reservation-customer-app',
-		'website-link': 'https://reservation-diner.netlify.app/',
 	},
 ];
 
